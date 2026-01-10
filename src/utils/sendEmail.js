7a43468e721c07d09c770c75f01715265abb49d7
vishalsingh455 <thevishalsingh79@gmail.com>
2026-01-10 15:11:52 +0530

    improving sendEmail feature

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -10,7 +10,7 @@ const sendEmail = async ({ to, subject, html }) => {
     })
 
     await transporter.sendMail({
-        from: `"Code Arena" <${process.env.EMAIL_USER}>`,
+        from: `"CODO" <${process.env.EMAIL_USER}>`,
         to,
         subject,
         html
